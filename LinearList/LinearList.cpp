c1b55af17faa9391b6ad8e0ffd11a0d1a8540e2b
landzee <chenzhijie03@qq.com>
2025-08-29 14:06:00 +0800

    change filename

---
+++ LinearList/LinearList.cpp
@@ -0,0 +1,181 @@
+﻿#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+//线性表中的顺序表
+//时间复杂度比链表高
+
+template <typename T>
+class LinearList {
+private:
+    T* data;           // 存储数据的数组
+    int capacity;      // 容量
+    int length;        // 当前长度
+
+public:
+    // 构造函数
+    //传参赋值默认值，调用时如果不输入参数就是默认值赋值
+    LinearList(int cap = 10) : capacity(cap), length(0) {
+        data = new T[capacity];
+    }
+
+    // 析构函数
+    ~LinearList() {
+        delete[] data;
+    }
+
+    // 拷贝构造函数
+    LinearList(const LinearList& other) : capacity(other.capacity), length(other.length) {
+        data = new T[capacity];
+        for (int i = 0; i < length; i++) {
+            data[i] = other.data[i];
+        }
+    }
+
+    // 赋值运算符重载
+    LinearList& operator=(const LinearList& other) {
+        if (this != &other) {
+            delete[] data;
+            capacity = other.capacity;
+            length = other.length;
+            data = new T[capacity];
+            for (int i = 0; i < length; i++) {
+                data[i] = other.data[i];
+            }
+        }
+        return *this;
+    }
+
+    // 获取线性表长度
+    int size() const {
+        return length;
+    }
+
+    // 判断线性表是否为空
+    bool empty() const {
+        return length == 0;
+    }
+
+    // 在指定位置插入元素
+    // 如果在第一个位置插入，那时间复杂度是O(n)
+    void insert(int index, const T& element) {
+        if (index < 0 || index > length) {
+            throw out_of_range("Index out of range");
+        }
+        if (length >= capacity) {
+            resize();
+        }
+        for (int i = length; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = element;
+        length++;
+    }
+
+    // 在末尾添加元素
+    void push_back(const T& element) {
+        insert(length, element);
+    }
+
+    // 删除指定位置的元素
+    void remove(int index) {
+        if (index < 0 || index >= length) {
+            throw out_of_range("Index out of range");
+        }
+        for (int i = index; i < length - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        length--;
+    }
+
+    // 获取指定位置的元素
+    T& get(int index) {
+        if (index < 0 || index >= length) {
+            throw out_of_range("Index out of range");
+        }
+        return data[index];
+    }
+
+    // 获取指定位置的元素（常量版本）
+    const T& get(int index) const {
+        if (index < 0 || index >= length) {
+            throw out_of_range("Index out of range");
+        }
+        return data[index];
+    }
+
+    // 查找元素，返回第一次出现的位置
+    int find(const T& element) const {
+        for (int i = 0; i < length; i++) {
+            if (data[i] == element) {
+                return i;
+            }
+        }
+        return -1; // 未找到返回-1
+    }
+
+    // 清空线性表
+    void clear() {
+        length = 0;
+    }
+
+    // 重载[]运算符
+    T& operator[](int index) {
+        return get(index);
+    }
+
+    // 重载[]运算符（常量版本）
+    const T& operator[](int index) const {
+        return get(index);
+    }
+
+    // 打印线性表内容
+    void display() const {
+        cout << "Linear List: ";
+        for (int i = 0; i < length; i++) {
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+
+private:
+    // 扩容函数
+    void resize() {
+        capacity *= 2;
+        T* newData = new T[capacity];
+        for (int i = 0; i < length; i++) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+    }
+};
+
+// 使用示例
+int main() {
+    LinearList<int> list;
+    
+    // 添加元素
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    
+    // 显示列表
+    list.display(); // 输出: Linear List: 1 2 3
+    // 插入元素
+    list.insert(1, 10);
+    list.display(); // 输出: Linear List: 1 10 2 3
+    
+    // 获取元素
+    cout << "Element at index 2: " << list.get(2) << endl; // 输出: Element at index 2: 2
+    
+    // 查找元素
+    int index = list.find(10);
+    cout << "Index of 10: " << index << endl; // 输出: Index of 10: 1
+    
+    // 删除元素
+    list.remove(0);
+    list.display(); // 输出: Linear List: 10 2 3
+    
+    return 0;
+}
---
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -0,0 +1,289 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class T>
+class Node{
+    private:
+        Node<T> *next;
+    public:
+        T data;
+
+        /**
+         * @brief 构造函数，用于创建一个新节点
+         * @param data 节点存储的数据值
+         * @return 无返回值
+         * 
+         * 该构造函数初始化节点的数据域和指针域，
+         * 将数据域设置为传入的参数值，将指针域初始化为空指针
+         */
+        Node(T data){
+                    this->data = data;
+                    this->next = NULL;
+        }
+
+        /**
+         * @brief 重载构造函数，用来初始化头节点。
+         */
+        Node() : data{}, next(nullptr) {}
+       
+         // 友元类，使链表类中成员函数有权访问私有成员变量
+        template<class T>
+        friend class SinglyLinkedList;
+};
+
+template<class T>
+class SinglyLinkedList{
+    private:
+        Node<T>* head = new Node<T>();//头指针位置为0，头插法插在head后面，位置为1
+    public:
+        SinglyLinkedList(){
+            head->next = NULL;
+        }
+
+        /**
+         * @brief 在链表头部后一位插入新节点
+         * head保持在第一位，头部插入在第二位。
+         * @param data 要插入的数据
+         */
+        void insertHead(T data){
+            Node<T>* newNode = new Node<T>(data);
+            newNode->next = head->next;
+            head->next = newNode;
+        }
+
+        /// @brief 在链表尾部插入新节点
+        /// @param data 要插入的数据
+        void insertTail(T data){ 
+            Node<T>* newNode = new Node<T>(data);
+            Node<T>* cur = head;
+            //用cur指针找到链表尾部
+            while(cur->next != NULL){
+                cur = cur->next;
+            }
+            cur->next = newNode;//在尾部插入新节点
+        }
+
+        void insertTail(Node<T>* newNode){ 
+            Node<T>* cur = head;
+            while(cur->next != NULL){
+                cur = cur->next;
+            }
+            cur->next = newNode;
+        }
+        /// @brief 在指定位置插入新节点
+        /// @param pos 要插入的位置
+        /// @param data 要插入的数据
+        void insert(int pos, T data){
+            if(pos<=0){
+                std::cout<<data<<"的插入位置不合法"<<std::endl;
+                return;
+            }
+            else{
+                Node<T>* newNode = new Node<T>(data);
+                Node<T>* cur = head;
+                for(int i=1; i<pos; i++){
+                    cur = cur->next;
+                    if(cur == NULL){
+                        std::cout<<data<<"的插入位置不合法"<<std::endl;
+                        return;
+                    }
+                }
+                newNode->next = cur->next;//将pos-1的next指针赋值给新节点的next指针
+                cur->next = newNode;
+            }
+
+            
+        }
+
+        /// @brief 删除指定位置的节点
+        /// @param pos 删除节点位置
+        void deleteNodePos(int pos){
+                Node<T>* cur = head;//head位置是0
+                if(pos <= 0){
+                    std::cout<<"删除位置不合法"<<std::endl;
+                    return;
+                }
+                else{
+                    for(int i = 1;i < pos;i++){
+                        if(cur->next == NULL){
+                            std::cout<<"删除位置不合法"<<std::endl;
+                            return;
+                        }
+                        cur = cur->next;
+                    }//选中要删除的节点的前一个节点
+                    Node<T>* del = cur->next;
+                    cur->next = del->next;
+                }
+        }
+
+        /// @brief 删除链表中所有指定元素
+        /// @param data 要删除的元素
+        void deleteNodeData(T data){ 
+            Node<T>* cur = head->next;
+            Node<T>* pre = head;
+
+            while(cur != NULL){
+                if(cur->data == data){
+                    pre->next = cur->next;
+                }
+                pre = cur;
+                cur = cur->next;
+            }
+        }
+
+        /**
+         * 查找链表中所有等于指定数据值的节点位置
+         * @param data 要查找的数据值
+         * @return 包含所有匹配节点位置的向量，从1开始计数
+         */
+        std::vector<int> findAllNodePos(T data) {
+            std::vector<int> positions;
+            Node<T>* cur = head->next;
+            int pos = 1;
+            // 遍历链表查找匹配的节点
+            while (cur != nullptr) {
+                // 如果当前节点数据匹配，记录位置
+                if (cur->data == data) {
+                    positions.push_back(pos);
+                }
+                cur = cur->next;
+                pos++;
+            }
+            return positions;
+        }
+
+        /**
+         * 修改链表中指定位置的元素值
+         * @param pos 要修改的元素位置，从1开始计数
+         * @param newData 新的元素值
+         */
+        void changeData(int pos, T newData){ 
+            Node<T>* cur = head;
+            for(int i=0; i<pos; i++){
+                cur = cur->next;
+                if(cur == NULL){
+                    std::cout<<"修改位置不合法"<<std::endl;
+                    return;
+                }
+            }
+            cur->data = newData;
+        }
+        /**
+         * 获取链表正数指定位置的数据元素
+         * @param pos 要获取数据的位置，从1开始计数
+         * @return 返回指定位置的数据元素
+         * @throws std::out_of_range 当位置不合法或超出链表长度时抛出异常
+         */    
+        T getData(int pos){
+            if(pos <= 0){
+                throw std::out_of_range("查找位置不合法");
+            }
+            Node<T>* cur = head;
+            for(int i = 0; i < pos; i++){
+                if(cur->next == nullptr){
+                    throw std::out_of_range("查找位置超过链表长度");
+                }
+                cur = cur->next;
+            }
+            return cur->data;
+        }
+
+        /**
+         * 获取链表倒数指定位置的数据元素
+         * @param tpos 要获取数据的倒数位置，从1开始计数，例如倒数第2
+         * @return 获取的倒数指定位置的数据元素
+         */
+        T getData_fromtail(int tpos){
+            Node<T>* fast = head->next;
+            Node<T>* slow = head->next;
+            for(int i = 0; i < tpos; i++){
+                if(fast->next == nullptr){
+                    throw std::out_of_range("查找位置超过链表长度");
+                }
+                fast = fast->next;
+            }
+            while(fast != nullptr){
+                fast = fast->next;
+                slow = slow->next;
+            }
+            return slow->data;
+        }
+
+        /**
+         * @brief 链表翻转
+         */
+        void overturn(){
+            Node<T>* pre = nullptr;
+            Node<T>* cur = head->next;
+            Node<T>* next = nullptr;
+            while(cur != nullptr){
+                next = cur->next;
+                cur->next = pre;
+                pre = cur;
+                cur = next;
+            }
+            head->next = pre;
+        }
+        /// @brief 输出链表
+        void printList(){
+            Node<T>* cur = head;
+            cur = cur->next;
+            while(cur != NULL){
+                std::cout<<cur->data<<" ";
+                cur = cur->next;
+            }
+            std::cout<<std::endl;
+        }
+
+        /**
+         * @brief 释放链表占用的内存空间
+         */
+        void release(){
+            Node<T>* cur = head;
+            while(cur != NULL){
+                Node<T>* next = cur->next;
+                delete cur;
+                cur = next;
+            }
+            head = NULL;
+        }
+};
+
+int main(){
+    SinglyLinkedList<std::string> book;
+    book.insertHead("aa");
+    book.insertHead("bb");
+    book.insertTail("cc");
+    book.insert(3,"dd");
+    book.insertTail("dd");
+    book.printList();
+
+    std::vector<int> positions = book.findAllNodePos("dd");
+
+    /** print all positions
+     * 初始化   |auto it = positions.begin()	|创建迭代器 it 指向容器起始位置
+     * 条件检查	|it != positions.end()	        |检查是否到达容器末尾
+     * 迭代	    |++it	                        |将迭代器移动到下一个元素
+     * 循环体	|std::cout << *it << " ";	    |输出当前元素值
+    */
+    for (auto it = positions.begin(); it != positions.end(); ++it) {
+    std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::string posdata = book.getData(2);
+    std::cout << posdata << std::endl;
+
+    std::string tposdata = book.getData_fromtail(3);
+    std::cout << tposdata << std::endl;
+
+    Node<std::string> *node = new Node<std::string>("ee");
+    book.insertTail(node);
+    book.changeData(3,"ii");
+    book.printList();
+
+    book.overturn();
+    book.printList();
+    book.release();
+    return 0;
+}
